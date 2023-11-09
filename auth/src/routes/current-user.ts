bf8c7bca3f7132a6e233bf362f0bd5ba1a42e22b
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2023-11-09 17:27:33 +0000

    tested sign up functionality

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,8 @@
 import express, {Request, Response} from "express"
-import jwt from "jsonwebtoken";
-import { requireAuth } from "../middlewares/require-auth";
 import { currentUser } from "../middlewares/current-user";
 const router = express.Router();
 
-router.get("/api/users/current-user", currentUser, requireAuth,(req: Request, res: Response) => {
+router.get("/api/users/current-user", currentUser,(req: Request, res: Response) => {
     res.send({ current_user: req.current_user || null });
 });
 
